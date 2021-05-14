7657672ec132eb6c1f93402a4cefb79442a60a2c
Leonardo Contreras <>
2021-05-14 13:04:17 -0600

    This is for Module 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Module
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 600;
+            int b = -55;
+            int c = 32;
+            int d = 0;
+
+            d = a + c + b;
+
+            Console.WriteLine(d);
+        }
+    }
+}
