646c8aee7408837c7bcf611c5c041b0cb32a301e
Leonardo Contreras <>
2021-05-14 17:18:04 -0600

    This is for Module 3.

---
+++ DTOS/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module.DTOS
+{
+    public class UserDTO
+    {
+        public UserDTO()
+        {
+        }
+        public string username { get; set; }
+
+        public string password { get; set; }
+
+        public string email { get; set; }
+
+    }
+
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Module.Models;
+using Module.DTOS;
 
 namespace Module
 {
@@ -11,11 +12,19 @@ namespace Module
             User myUser = new User();
             myUser.username = "Totally_Cool_Dude";
             myUser.password = "1234567890";
-            myUser.email = "T0tallyC00l@yahoo.com";
+            
 
-            Console.WriteLine(myUser.username);
-            Console.WriteLine(myUser.password);
-            Console.WriteLine(myUser.email);
+
+            UserDTO newDTOUser = new UserDTO();
+            newDTOUser.username = "Totally_Cool_Dude";
+            newDTOUser.password = "1234567890";
+            
+
+            Console.WriteLine(newDTOUser.username);
+            Console.WriteLine(newDTOUser.password);
+            
+
+
         }
 
          
