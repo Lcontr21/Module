90c6b856e3645f4118a1327941f2a8c5de0b92fc
Leonardo Contreras <>
2021-05-20 15:05:11 -0600

    This is for Module 5.

--- Program.cs
+++ Program.cs
@@ -7,29 +7,42 @@ namespace Module
     class Program
     {
        
-        static void Main(string[] args)
+        static void Main()
         {
-            User myUser = new User();
-            myUser.username = "Totally_Cool_Dude";
-            myUser.password = "1234567890";
+            //User myUser = new User();
+            //myUser.username = "Totally_Cool_Dude";
+            //myUser.password = "1234567890";
 
 
-            UserDTO newDTOUser = new UserDTO();
-            newDTOUser.username = "Totally_Cool_Dude";
-            newDTOUser.password = "1234567890";
+            //UserDTO newDTOUser = new UserDTO();
+            //newDTOUser.username = "Totally_Cool_Dude";
+            //newDTOUser.password = "1234567890";
 
-            Console.WriteLine(newDTOUser.username);
-            Console.WriteLine(newDTOUser.password);
-
-
+            //Console.WriteLine(newDTOUser.username);
+            //Console.WriteLine(newDTOUser.password);
 
 
+            //DateTime myDateTimeVariable = new DateTime(2003, 06, 17, 15, 1, 0);
+            //DateTime myDateTimeVariable2 = new DateTime(1980, 04, 21, 9, 0, 53);
+
+            //if (myDateTimeVariable < myDateTimeVariable2) 
+            //{
+            //    Console.WriteLine("Its True");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Its false");
+            //}
 
 
+            for (int x=0;x<210;x++)
+            {
+                Console.WriteLine("Line:" + x);
+            }
 
+            
         }
-    }
 
-    
-    
+    }    
 }
+
