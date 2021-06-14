a0d6a6bacb8867d66aa59fc43e56d61a3fe4d292
Leonardo Contreras <>
2021-06-14 13:01:38 -0600

    This is the 3rd revision for Module 4.

--- Program.cs
+++ Program.cs
@@ -8,23 +8,24 @@ namespace Module
 {
     class Program
     {
-
         static void Main()
         {
-            RegularClass myNewClass = new RegularClass();
+            DateTime myDateTimeVariable = new DateTime(2003, 06, 17, 15, 1, 0);
+            DateTime myDateTimeVariable2 = new DateTime(1980, 04, 21, 9, 0, 53);
 
-            myNewClass.PrintsSomethingToTheConsole<string>(345678, "This is the 1st string", "This is the 2nd string");
+            int result = DateTime.Compare(myDateTimeVariable, myDateTimeVariable2);
+
+            Console.WriteLine(result);
+
+            if(result==1)
+            {
+                Console.WriteLine("The first date is greater");
+            }
+            else
+            {
+                Console.WriteLine("The second date is greater");
+            }
         }
     }
 }
 
-public class RegularClass
-{
-    public void PrintsSomethingToTheConsole<T>(int parameter1, string parameter2, T parameter3)
-    {
-        Console.WriteLine("The values of the parameters are the following:");
-        Console.WriteLine(parameter1);
-        Console.WriteLine(parameter2);
-        Console.WriteLine(parameter3);
-    }
-}
